1960008d8aac568cff478c37293572e2718e6d7e
霍振鹏 <huozhenpengdyx@gmail.com>
2019-11-08 16:57:57 +0800

    首页接口、广告栏

--- lib/home_pages/home_page.dart
+++ lib/home_pages/home_page.dart
@@ -1,11 +1,66 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_shop/http/dio_agent.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 class HomePage extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Text("首页"),
+    ScreenUtil.instance=ScreenUtil(width: 750,height: 1334)
+      ..init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("百姓生活+"),
+      ),
+      body: FutureBuilder(
+        future: getHomeContent(),
+          builder: (context,snapshot)
+              {
+                if(snapshot.hasData)
+                  {
+                    var data=json.decode(snapshot.data.toString());
+                    List<Map> swiperDataList=(data['data']['slides'] as List).cast();
+                    return Column(
+                      children: <Widget>[
+
+                        SwiperWidget(items: swiperDataList)
+                      ],
+                    );
+                  }
+                else
+                  {
+                    return Container(
+                      alignment: Alignment.topCenter,
+                      //loading效果
+                      child: Text("加载中。。。"),
+                    );
+                  }
+              }
+      ),
+    );
+  }
+}
+
+class SwiperWidget extends StatelessWidget
+{
+  SwiperWidget({Key key,this.items}):super(key:key);
+  List<Map> items;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil.instance.setWidth(750),
+      height: ScreenUtil.instance.setHeight(333),
+      child: Swiper(
+        itemCount: items.length,
+        autoplay: true,
+        //指示条
+        pagination: SwiperPagination(),
+        itemBuilder: (context,index){
+          return Image.network("${items[index]['image']}",fit: BoxFit.fill);
+        },
+      ),
     );
   }
   
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/splansh_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: '百姓生活购物',
       theme: ThemeData(
