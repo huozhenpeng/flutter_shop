9ca7a2e16d2d285b55de51d2ae2d192d0e7ef3bd
霍振鹏 <huozhenpengdyx@gmail.com>
2019-11-08 20:57:15 +0800

    首页商品推荐、商品种类

--- lib/home_pages/home_page.dart
+++ lib/home_pages/home_page.dart
@@ -8,8 +8,6 @@ class HomePage extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance=ScreenUtil(width: 750,height: 1334)
-      ..init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("百姓生活+"),
@@ -22,11 +20,20 @@ class HomePage extends StatelessWidget
                   {
                     var data=json.decode(snapshot.data.toString());
                     List<Map> swiperDataList=(data['data']['slides'] as List).cast();
-                    return Column(
-                      children: <Widget>[
+                    String info="商品推荐";
+                    List<Map> products=(data['data']['recommend'] as List).cast();
 
-                        SwiperWidget(items: swiperDataList)
-                      ],
+                    List<Map> recomands=(data['data']['category'] as List).cast();
+
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          SwiperWidget(items: swiperDataList),
+                          ProductCategroysWidget(info: info,datas:products),
+                          CategoryWidget(datas: recomands),
+
+                        ],
+                      ),
                     );
                   }
                 else
@@ -42,7 +49,7 @@ class HomePage extends StatelessWidget
     );
   }
 }
-
+//banner
 class SwiperWidget extends StatelessWidget
 {
   SwiperWidget({Key key,this.items}):super(key:key);
@@ -51,7 +58,7 @@ class SwiperWidget extends StatelessWidget
   Widget build(BuildContext context) {
     return Container(
       width: ScreenUtil.instance.setWidth(750),
-      height: ScreenUtil.instance.setHeight(333),
+      height: ScreenUtil.instance.setHeight(300),
       child: Swiper(
         itemCount: items.length,
         autoplay: true,
@@ -62,6 +69,176 @@ class SwiperWidget extends StatelessWidget
         },
       ),
     );
+  }
+}
+
+///listview
+class ProductCategroysWidget extends StatelessWidget
+{
+  String info;
+  List<Map> datas;
+  ProductCategroysWidget({Key key,this.info,this.datas}):super(key:key);
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: <Widget>[
+        _getRecommand(info),
+        _getListItems(datas)
+
+      ],
+    );
+  }
+  ///推荐
+  Widget _getRecommand(String info)
+  {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      padding: EdgeInsets.all(12),
+      alignment: Alignment.centerLeft,
+      child: Text(info,style: TextStyle(color: Colors.lightBlueAccent)),
+      decoration: BoxDecoration(border: Border(bottom: BorderSide(width: 1,color: Colors.black12))),
+    );
+  }
+
+  ///商品列表
+  Widget _getListItems(List<Map> datas)
+  {
+    return Container(
+      child: ListView.builder(itemBuilder: (context,index){
+        return _getItem(datas[index]);
+      },
+        scrollDirection: Axis.horizontal,
+        itemCount: datas.length,
+      ),
+      //这儿如果没有定义高度，会报错：18:57:07.164 4 info flutter.tools I/flutter ( 8817): Horizontal viewport was given unbounded height.
+      height: ScreenUtil.instance.setHeight(320),
+      width: ScreenUtil.instance.setWidth(750),
+    );
+  }
+
+  ///商品种类item
+  Widget _getItem(Map data)
+  {
+    return InkWell(
+
+      onTap: (){
+
+      },
+
+      child: Container(
+        color: Colors.white,
+        width: ScreenUtil.instance.setWidth(250),
+        height: ScreenUtil.instance.setHeight(300),
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(border: Border(left: BorderSide(width: 1.0,color: Colors.black12))),
+              child: Image.network(data['image']),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 20),
+              child: Text(
+                "${data['mallPrice']}",
+                style: TextStyle(
+                  color: Colors.black
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 10),
+              child: Text(
+                "${data['price']}",
+                style: TextStyle(
+                    color: Colors.black26,
+                   decoration: TextDecoration.lineThrough
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+///gridview
+class CategoryWidget extends StatelessWidget
+{
+  CategoryWidget({Key key,this.datas}):super(key:key);
+  List<Map> datas;
+  @override
+  Widget build(BuildContext context) {
+    if(datas.length>10)
+      {
+       datas.removeLast();
+      }
+    return Container(
+        margin: EdgeInsets.only(top: 10),
+        color: Colors.red,
+        width: ScreenUtil.instance.setWidth(750),
+        height: ScreenUtil.instance.setHeight(350),
+        //padding: EdgeInsets.all(6.0),
+//这个会报错，就是返回了11条数据，没行占5个
+//        child: GridView.builder(
+//            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+//            itemBuilder: (context,index){
+//              return _getItemWidget(datas[index]);
+//        }),
+      child: GridView.count(
+        //注意这个值很重要
+        childAspectRatio: 0.7,
+        crossAxisSpacing: 6.0,
+        mainAxisSpacing: 10.0,
+        crossAxisCount: 5,
+        children:datas.map((element){
+         return _getItemWidget(element);
+         }
+      ).toList(),
+      ),
+      )
+    ;
+  }
+  
+  Widget  _getItemWidget(Map map)
+  {
+    return InkWell(
+      child:Container(
+        color: Colors.lightBlue,
+        child:Column(
+          children: <Widget>[
+            _getImages(map['image']),
+            Text(map['mallCategoryName'])
+          ],
+        ) ,
+      ),
+      onTap: (){
+
+      },
+    );
+  }
+  
+  Widget _getImages(String url)
+  {
+
+    ///ClipOval没有起到作用，先放下
+    return ClipOval(
+        child:Container(
+          //color: Colors.black12,
+          alignment: Alignment.center,
+          //padding: EdgeInsets.all(6.0),
+          width: ScreenUtil.instance.setWidth(120),
+          height: ScreenUtil.instance.setHeight(120),
+          child: Image.network(
+            url,
+            fit: BoxFit.fitWidth,
+          ),
+        )
+    );
+
+
   }
   
 }
--- lib/splansh_page.dart
+++ lib/splansh_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/main_page.dart';
 
 class SplanshWidget extends StatefulWidget
@@ -36,6 +37,9 @@ class _SplanshWidgetState extends State<SplanshWidget> with SingleTickerProvider
   }
   @override
   Widget build(BuildContext context) {
+    //这个初始化最早能提取到这儿
+    ScreenUtil.instance=ScreenUtil(width: 750,height: 1334)
+      ..init(context);
     return FadeTransition(
         opacity: _animation,
         child: Image.network("https://img0.sc115.com/uploads3/sc/psd/181222/1812220677256.jpg",fit: BoxFit.fill,),
